bb758960049bcb4d3d022780dc5b99e541749957
thawzinag <37143924+ThawZinA@users.noreply.github.com>
2025-08-10 12:03:26 +0100

    Added banner

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
-import GuessInput from '../GuessInput/GuessInput';
-import GuessList from '../GuessList/GuessList';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import GuessInput from "../GuessInput/GuessInput";
+import GuessList from "../GuessList/GuessList";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import WonBanner from "../WonBanner/WonBanner";
+import LostBanner from "../LostBanner/LostBanner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
+// To make debugging easier, we'll log the solution in the console.s
 console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [status, setStatus] = React.useState("running");
+
   function handleSubmitGuesses(newGuess) {
     // Add the new guess to the list of guesses.
-    setGuesses([...guesses, newGuess]);
+    const nextGuesses = [...guesses, newGuess];
+    setGuesses(nextGuesses);
+
+    if (newGuess.toUpperCase() === answer) {
+      setStatus("won");
+    } else if (nextGuesses.length > NUM_OF_GUESSES_ALLOWED) {
+      setStatus("lost");
+    }
   }
-  return <>
-    <GuessList guesses={guesses} answer={answer} />
-    <GuessInput handleSubmitGuesses={handleSubmitGuesses} />
-  </>;
+  return (
+    <>
+      <GuessList guesses={guesses} answer={answer} />
+      <GuessInput
+        gameStatus={status}
+        handleSubmitGuesses={handleSubmitGuesses}
+      />
+      {status === "won" && <WonBanner numOfGuesses={guesses.length} />}
+      {status === "lost" && <LostBanner answer={answer} />}
+    </>
+  );
 }
 
 export default Game;
